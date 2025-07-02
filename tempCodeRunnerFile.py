5cc45bbf91606011d638c02ed97f19e10b257ff0
Hrishi-Shekhar <hrishi08072005@gmail.com>
2025-07-02 20:55:38 +0530

    minute changes

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-print("[INFO] Applying PCA to reduce dimensionality...")
-pca = PCA(n_components=128, random_state=42)
-train_features = pca.fit_transform(train_features)
-val_features = pca.transform(val_features)
