795d7dffb0ba3cb11bd23fe152d2898c7f4036c4
Hrishi-Shekhar <hrishi08072005@gmail.com>
2025-07-11 01:46:11 +0200

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from task_a import run_pipeline as run_task_a
 from task_b import run_siamese_pipeline as run_task_b
 
 if _name_ == "_main_":
-    # Set your dataset directories here
-    test_dir_task_a = "/path/to/test_images_task_a"  # Task A test images dir
-    test_dir_task_b = "/path/to/test_images_task_b"  # Task B test images dir
+    
+    test_dir_task_a = None  # Replace with task A test images dir path
+    test_dir_task_b = None  # Replace with task B test images dir path
 
     print("\n=== Running Task A Pipeline ===")
     run_task_a(test_dir=test_dir_task_a)
